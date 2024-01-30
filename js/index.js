4f94e2add31b9365918cde1efe6afba2950b201e
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-01-30 14:44:50 +0200

    Update

--- js/index.js
+++ js/index.js
@@ -1,12 +1,63 @@
 var image;
 var greyImage;
+var redImage;
+var rainbowImage;
+var imgCanvas = document.getElementById("main-canvas");
+var fileInput = document.getElementById("load-button");
 
 function upload() {
-    var imgCanvas = document.getElementById("main-canvas");
-    var fileInput = document.getElementById("load-button");
-    image = new SimpleImage(fileInput);
-    image = new SimpleImage(fileInput);
-    image = new SimpleImage(fileInput);
     image = new SimpleImage(fileInput);
     image.drawTo(imgCanvas);
 }
+
+function makeGrey() {
+    for (var pixel of image.values()) {
+        var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
+        pixel.setRed(avg);
+        pixel.setGreen(avg);
+        pixel.setBlue(avg);
+    }
+    greyImage = new SimpleImage(fileInput);
+    image.drawTo(imgCanvas);
+}
+
+
+function makeRed() {
+    for (var pixel of image.values()) {
+        pixel.setRed(255);
+    }
+    redImage = new SimpleImage(fileInput);
+    image.drawTo(imgCanvas);
+}
+
+function makeRainbow() {
+    for (var pixel of image.values()) {
+        pixel.setRed(255);
+    }
+    rainbowImage = new SimpleImage(fileInput);
+    image.drawTo(imgCanvas);
+    
+}
+
+function reset() {
+    var greyImage;
+    var redImage;
+    var rainbowImage;
+    
+}
+
+// function imageIsLoaded() {
+//     if (image === null || ! image.complete()) {
+//         alert("Photo not loaded");
+//      }
+// }
+
+
+
+
+// if (fgImage === null || ! fgImage.complete()) {
+//     alert("Foreground not loaded");
+// }
+// if (bgImage === null || ! bgImage.complete()) {
+//     alert("Background not loaded");
+// }
